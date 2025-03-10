61a6e8559a73a36fababbaffa1411412b531bb3c
Lutfi Fahreza <lutfifahreza84.com>
2025-03-10 17:11:32 +0700

    Pesan commit yang jelas

--- database/migrations/2025_03_10_165945_create_suppliers_table.php
+++ database/migrations/2025_03_10_165945_create_suppliers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateSuppliersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name'); // Nama supplier
+            $table->string('contact_info')->nullable(); // Informasi kontak, bisa null
+            $table->timestamps(); // Kolom created_at dan updated_at
         });
     }
 
@@ -26,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('suppliers');
     }
-};
+}
